168782b269e7d2364cc3487b3430383dfa003d2a
ManuErCaximba <ercaximbero96@gmail.com>
2020-09-07 13:31:04 +0200

    heroku me cago en todo

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,7 @@ SECRET_KEY = '^##ydkswfu0+=ofw0l#$kv^8n)0$i(qd&d&ol#p9!b$8*5%j1+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
@@ -68,7 +69,19 @@ MODULES = [
     'voting',
 ]
 
-BASEURL = 'http://localhost:8000'
+BASEURL = 'https://decide-07092020.herokuapp.com'
+
+APIS = {
+    'authentication': 'https://decide-07092020.herokuapp.com',
+    'base': 'https://decide-07092020.herokuapp.com',
+    'booth': 'https://decide-07092020.herokuapp.com',
+    'census': 'https://decide-07092020.herokuapp.com',
+    'mixnet': 'https://decide-07092020.herokuapp.com',
+    'postproc': 'https://decide-07092020.herokuapp.com',
+    'store': 'https://decide-07092020.herokuapp.com',
+    'visualizer': 'https://decide-07092020.herokuapp.com',
+    'voting': 'https://decide-07092020.herokuapp.com',
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -160,3 +173,4 @@ except ImportError:
 
 
 INSTALLED_APPS = INSTALLED_APPS + MODULES
+django_heroku.settings(locals())
